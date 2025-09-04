305e9c6d9d9e8a76d683d303f03f70ba7abcc4bf
LooseCannon0 <isaacsmith357@gmail.com>
2025-09-04 10:54:31 -0400

    Update setup.sh

--- lixunx_setup/setup.sh
+++ lixunx_setup/setup.sh
@@ -21,17 +21,20 @@ fi
 sudo apt-get install -y nfs-common
 
 # Install Zabbix agent from system repository
-sudo apt-get install -y zabbix-agent
+wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_7.0-2+ubuntu24.04_all.deb
+sudo dpkg -i zabbix-release_7.0-2+ubuntu24.04_all.deb
+sudo apt update
+sudo apt install -y zabbix-agent2
 # Enable and start Zabbix agent service
-sudo systemctl enable zabbix-agent
-sudo systemctl start zabbix-agent
+sudo systemctl enable zabbix-agent2
+sudo systemctl start zabbix-agent2
 # Configure Zabbix agent
 ZABBIX_SERVER="192.168.57.15"  # Replace with your Zabbix server address
-sudo sed -i "s/^Server=.*/Server=$ZABBIX_SERVER/"  /etc/zabbix/zabbix_agentd.conf
-sudo sed -i "s/^ServerActive=.*/ServerActive=$ZABBIX_SERVER/"  /etc/zabbix/zabbix_agentd.conf
-sudo sed -i "s/^# HostnameItem=.*/HostnameItem=system.hostname/"  /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/^Server=.*/Server=$ZABBIX_SERVER/"  /etc/zabbix/zabbix_agent2.conf
+sudo sed -i "s/^ServerActive=.*/ServerActive=$ZABBIX_SERVER/"  /etc/zabbix/zabbix_agent2.conf
+sudo sed -i "s/^# HostnameItem=.*/HostnameItem=system.hostname/"  /etc/zabbix/zabbix_agent2.conf
 
 # Restart Zabbix agent service to apply changes
-sudo systemctl restart zabbix-agent
+sudo systemctl restart zabbix-agent2
 
 echo "Base configuration complete."
